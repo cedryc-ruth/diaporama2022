09eec4e4d8786268fdb91c8047a1a987713a1d7b
Cédryc Ruth <ceruth@epfc.eu>
2022-02-24 16:27:49 +0100

    Animation du diaporama en JS

--- js/app.js
+++ js/app.js
@@ -1,2 +1,47 @@
 /* Functions JS */
-console.log('ok');
+const liste = [
+	'images/landscape1.jpg',
+	'images/landscape2.jpg',
+	'images/landscape3.jpg',
+	'images/landscape4.jpg',
+];
+
+let position = 0;
+
+const cadre = document.getElementById('cadre');
+
+const img = cadre.getElementsByTagName('img')[0];
+const figcaption = cadre.getElementsByTagName('figcaption')[0];
+
+const btPrev = document.getElementById('btPrev');
+const btNext = document.getElementById('btNext');
+
+btNext.onclick = function() {
+	if(position==liste.length-1) {
+		position = -1;
+	}
+	
+	img.src = liste[++position];
+	img.alt = 'Autre chose';
+	figcaption.innerText = 'Autre chose';
+};
+
+btPrev.onclick = function() {
+	if(position==0) {
+		position = liste.length;
+	}
+	
+	img.src = liste[--position];
+	img.alt = 'Autre chose';
+	figcaption.innerText = 'Autre chose';
+};
+
+
+
+
+
+
+
+
+
+
